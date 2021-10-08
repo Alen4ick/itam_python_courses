bf6088385b5b0d2b13f80748a0da2b42e1f0b13d
Alen4ick <90774403+Alen4ick@users.noreply.github.com>
2021-10-08 18:28:20 +0300

    Update task_2_1_2.py

--- homeworks/chapter_2/task_2_1_2.py
+++ homeworks/chapter_2/task_2_1_2.py
@@ -1,7 +1,6 @@
 n = int(input())
 s = [int(i) for i in input().split()]
 def summation(m):
-    m = [m[i] if m[i] > 0 else abs(m[i])*2 for i in range(len(m))]
+    m = [i if i > 0 else abs(i)*2 for i in m]
     maxim = max(m)
     return sum([x/maxim for x in m])
-print(summation(s))
